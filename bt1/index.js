f0acb9b2096206c3eb8b7d2d0c333cd01b5bbf6b
viktornguyen0210 <nhv202@gmail.com>
2023-08-21 23:55:24 +0700

    update exercise

---
+++ bt1/index.js
@@ -0,0 +1,20 @@
+var btnTong = document.getElementById("btnTong")
+btnTong.onclick = function (){
+
+var luong = document.getElementById("luong").value;
+
+var ngay = document.getElementById("ngay").value;
+
+luong = Number(luong);
+ngay = Number(ngay);
+
+var tong = luong * ngay;
+console.log(tong);
+
+var ketQua = "Kết quả: " + tong
+
+var result = document.getElementById("result");
+
+result.innerText = ketQua;
+
+}
